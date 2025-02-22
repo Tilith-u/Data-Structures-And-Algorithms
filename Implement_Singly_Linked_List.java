6929691caaa82d5efebcc1c6dfcb683ff398af28
Tilith-u <tilithvinsara78@gmail.com>
2025-02-22 17:15:50 +0530

    Add files via upload

---
+++ Implement_Singly_Linked_List.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package implement_singly_linked_list;
+
+/**
+ *
+ * @author User
+ */
+public class Implement_Singly_Linked_List {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
